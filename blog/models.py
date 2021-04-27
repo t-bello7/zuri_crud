e9a88493820595b0ecee504104b9cec4d8f711a8
tomi <bello4aus@gmail.com>
2021-04-27 11:53:06 +0100

    authentication worked on , still having issues with the heroku

---
+++ blog/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import datetime, date
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=200, default='wooha')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('article_details',args=[self.id])
+
+class comments(models.Model):
+    post = models.ForeignKey(Post, related_name= "comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
+
+    
---
+++ blog/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, comments
+from django.urls import reverse_lazy
+from django.views import generic
+from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login
+from .forms import UserCreateForm, CommentForm
+
+# Create your views here.
+#def home(request):
+#    return render(request,'home.html',{})
+#homeView for list
+class HomeView(ListView):
+    model = Post
+    template_name= 'user.html'
+    ordering = ['-id']
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'update.html'
+    fields=['title','title_tag','body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('user')
+
+class AddCommentView(CreateView):
+    model = comments
+    form_class = CommentForm
+    template_name = 'add_comments.html'
+
+    def form_valid(self, form):
+        form.instance.post.id = self.kwargs['pk']
+        return super().form_valid(form)
+    success_url = reverse_lazy('user')
+
+
+def signup(request):
+    form_class = UserCreateForm
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1']
+            )
+            login(request, new_user)
+            return redirect('user')
+    else:
+        form = UserCreateForm()
+    return render(request, 'signup.html', {'form': form})
+
