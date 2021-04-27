b8d9f54b31fff0abd31d506e329079dde756c078
tomi <bello4aus@gmail.com>
2021-04-27 20:21:37 +0100

    settings

--- zuri_crud/settings.py
+++ zuri_crud/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
