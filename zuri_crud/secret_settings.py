7eac70ad3689befa3024c1fb00e5cae592eee858
Bello Oluwatomisin <bello4aus@gmail.com>
2021-04-27 12:45:56 +0100

    Delete secret_settings.py

--- zuri_crud/secret_settings.py
+++
@@ -1,6 +0,0 @@
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n=7!_2#b_60^^rc9+y1lby8z=w!mzqt4l*y*^iw!k5i5k&b9j%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
